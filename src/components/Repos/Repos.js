bff21d054973cfd65606a9a33a5560234f3991ed
TsengMingYu <qazwsx9006@hotmail.com>
2016-10-31 21:32:00 +0800

    嘗試使用 class 取代 靜態

--- src/components/Repos/Repos.js
+++ src/components/Repos/Repos.js
@@ -1,11 +1,31 @@
 import React from 'react';
 
-const Repos = (props) => (
-  <div>
-    <h3>Repos</h3>
-    <h5>{props.params.name}</h5>
-  </div>
-);
+// const Repos = (props) => (
+//   <div>
+//     <h3>Repos</h3>
+//     <h5>{props.params.name}</h5>
+//   </div>
+// );
+
+// Repos.propTypes = {
+//   params: React.PropTypes.object,
+// };
+
+class Repos extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+
+  render() {
+    return (
+      <div>
+        <h3>Repos</h3>
+        <h5>{this.props.params.name}</h5>
+      </div>
+    )
+  }
+}
 
 Repos.propTypes = {
   params: React.PropTypes.object,
